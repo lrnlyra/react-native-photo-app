1ddcc8539d12a7cf8a510774238cd4441f319f56
Lyra <laurene.v@outlook.com>
2017-04-07 00:17:18 +0200

    add camera + taking & saving a picture

--- index.android.js
+++ index.android.js
@@ -6,48 +6,78 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
+import Camera from 'react-native-camera';
 
 export default class ReactNativePhotoApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>
+					First picture app
+				</Text>
+				<Text style={styles.instructions}>
+					You can now take a picture :
+				</Text>
+				<View style={styles.cameraContainer}>
+					<Camera
+						ref={(cam) => {
+							this.camera = cam;
+						}}
+						style={styles.cameraPreview}
+						aspect={Camera.constants.Aspect.fill}>
+						<Text style={styles.cameraCapture} onPress={this.takePicture.bind(this)}></Text>
+					</Camera>
+				</View>
+			</View>
+		);
+	}
+	takePicture() {
+		const options = {};
+		//options.location = ...
+		this.camera.capture({metadata: options})
+		.then((data) => console.log(data))
+		.catch(err => console.error(err));
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		backgroundColor: '#dd9e00',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333'
+	},
+	cameraContainer: {
+		flex: 1,
+		margin: 15
+	},
+	cameraPreview: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		alignItems: 'center'
+	},
+	cameraCapture: {
+		backgroundColor: '#dd9e00',
+		borderRadius: 45,
+		width: 45,
+		height: 45,
+		marginBottom: 20,
+		borderWidth: 2,
+		borderColor: '#fff'
+	}
 });
 
 AppRegistry.registerComponent('ReactNativePhotoApp', () => ReactNativePhotoApp);
